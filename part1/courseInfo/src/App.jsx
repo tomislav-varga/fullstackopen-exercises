7b6cc8ded1ac14f43da11c9def8d23bdbc26128e
tomi <tomislav.varga@icloud.com>
2024-04-28 12:24:06 +0200

    solution of exercise 1.1

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,3 +1,26 @@
+const Header = (props) => {
+  return (
+    <>
+          <h1>{props.course}</h1></>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+          <p>Number of exercises {props.totalOfExercises}</p></>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -7,20 +30,13 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const totalOfExercises = exercises1 + exercises2 + exercises3
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2 part3={part3} exercises3={exercises3} />
+      <Total totalOfExercises={totalOfExercises} />
     </div>
   )
 }
