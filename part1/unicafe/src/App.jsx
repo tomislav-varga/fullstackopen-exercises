ffea233ca11db4a9e772f8ab623f7b9398be7f9a
tomi <tomislav.varga@icloud.com>
2024-05-12 11:02:27 +0200

    start exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,6 +13,18 @@ const Statistics = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  /// ...
+  return(
+    <div>
+      <StatisticLine text="good" value ={...} />
+      <StatisticLine text="neutral" value ={...} />
+      <StatisticLine text="bad" value ={...} />
+      // ...
+    </div>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
