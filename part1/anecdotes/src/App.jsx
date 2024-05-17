676d903c1e2ea51d619a8a415850227acfdfd340
tomi <tomislav.varga@icloud.com>
2024-05-17 12:50:23 +0200

    solution exercise 1.12

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -17,7 +17,10 @@ const App = () => {
   
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
+        Next anecdote
+      </button>
     </div>
   )
 }
