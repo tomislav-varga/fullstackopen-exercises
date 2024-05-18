c5a366117cc52f364b2c0bc3734559928d9afbce
tomi <tomislav.varga@icloud.com>
2024-05-18 10:43:15 +0200

    solution exercise 2.1

--- part2/courseInfo/src/components/Course.jsx
+++ part2/courseInfo/src/components/Course.jsx
@@ -1,5 +1,34 @@
-const Course = () => {
-    return ()
+const Header = (props) => {
+    return (
+      <>
+            <h1>{props.course}</h1></>
+    )
+  }
+  
+const Part = (props) => {
+return (
+    <>
+        <p>{props.part} {props.exercises}</p>
+    </>
+)
+}
+  
+const Content = (props) => {
+    return (
+        <div>
+            {props.parts.map((part, index) => (
+                <Part key={index} part={part.name} exercises={part.exercises} />
+            ))}
+        </div>
+    )
+}
+const Course = ( {course}) => {
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+        </div>
+    )
 }
 
 export default Course
