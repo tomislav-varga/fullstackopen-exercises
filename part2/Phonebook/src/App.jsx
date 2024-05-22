6ea9e115f5e4ba8ba761c40e27cffb106f2fbecc
tomi <tomislav.varga@icloud.com>
2024-05-22 13:27:14 +0200

    code-explanation-exercise-2.9

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -64,7 +64,22 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.filter(person => person.name.toLowerCase().includes(searchQuery.toLowerCase())
+        {/* persons.filter(...): This method is called on the persons array. 
+        The filter method creates a new array with all elements that pass the 
+        test implemented by the provided function */}
+      {persons.filter(person => {
+        /* This is the function provided to filter. For each person object in the 
+        persons array, it converts both the person's name and the search query to 
+        lowercase (to make the search case-insensitive) and then checks if the 
+        person's name includes the search query string. 
+        The includes method is used to determine whether the person's name contains 
+        the search query substring. It returns true if the substring is found 
+        anywhere within the given string; otherwise, it returns false.*/
+        return person.name.toLowerCase().includes(searchQuery.toLowerCase())
+      }
+      /*The result of the filter method is an array of person objects whose names 
+      match the search query. This filtered array is then mapped to <li> elements that
+      are displayed in the <ul> list.  */
       ).map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
