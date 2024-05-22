e22132fdadabb9aa9d3ce5c67090c67c281e15c7
tomi <tomislav.varga@icloud.com>
2024-05-22 10:35:29 +0200

    code-explanation-exercise-2.6

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -9,12 +9,15 @@ const App = () => {
   const [persons, setPersons] = useState(personsArray)
   const [newName, setNewName] = useState('')
 
+  // This is the function that is executed when the form is submitted.
   const addName = (event) => {
-    event.preventDefault()
-    setPersons(persons.concat({ name: newName }))
-    setNewName('')
+    event.preventDefault() // Prevents the default behavior of the form, to reload the 
+                           // page.
+    setPersons(persons.concat({ name: newName })) // Adds the new person to the persons array.
+    setNewName('') // Sets the new name to an empty string.
   }
 
+  // This is the function that is executed when the user types in the input field.
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -23,8 +26,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      {/* This is the container for the form. The onSubmit attribute is an event handler 
+      that specifies a function (addName) to execute when the form is submitted */}
       <form onSubmit={addName}>
         <div>
+          {/* This input field is for the user to enter a new name. The value attribute 
+          is bound to the newName state, meaning the input field displays the value of 
+          newName. The onChange attribute is an event handler that updates the newName 
+          state with the input's current value whenever the user types in the field, 
+          using the handleNameChange function. */}
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
