1189135300815d965e272756c3da614134308e62
tomi <tomislav.varga@icloud.com>
2024-05-23 10:11:58 +0200

    solution exercise 2.11

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const personsArray = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
   
-  const [persons, setPersons] = useState(personsArray)
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setNewSearch] = useState('')
+
+  useEffect(() => { 
+    axios
+     .get('http://localhost:3001/persons')
+     .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  }, [])
 
   const addNameAndNumber = (event) => {
     event.preventDefault()
