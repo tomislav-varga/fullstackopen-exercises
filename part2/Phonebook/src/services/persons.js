7d44c79fd88ed886039eb615c5a702f3383414c7
tomi <tomislav.varga@icloud.com>
2024-05-25 15:39:12 +0200

    start exercise 2.14

--- part2/Phonebook/src/services/persons.js
+++ part2/Phonebook/src/services/persons.js
@@ -5,12 +5,19 @@ const getAll = () => {
     const promise = axios.get(baseURL)
     return promise.then(response => response.data)
   }
-  const create = newObject => {
+
+const create = newObject => {
     const promise = axios.post(baseURL,newObject)
+return promise.then(response => response.data)
+}
+
+const deletePerson = id => {
+    const promise = axios.delete(`${baseURL}/${id}`)
     return promise.then(response => response.data)
-  }
+}
 
 export default {
     getAll,
     create,
+    deletePerson
 }
