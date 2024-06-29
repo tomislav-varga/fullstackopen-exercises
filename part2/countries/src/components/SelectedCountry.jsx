8eaa044ab0d7b60dc865aa54e30bc0b35c9dd1ea
tomi <tomislav.varga@icloud.com>
2024-06-29 09:09:28 +0200

    solution exercise 2.20

--- part2/countries/src/components/SelectedCountry.jsx
+++ part2/countries/src/components/SelectedCountry.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+const api_key = import.meta.env.VITE_KEY
 
 const SelectedCountry = ( {selectedCountry} ) => {
+
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {
+        if (selectedCountry) {
+          const capital = selectedCountry.capital
+          const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+    
+          fetch(weatherUrl)
+            .then(response => response.json())
+            .then(data => {
+              setWeather(data)
+            })
+            .catch(error => console.error("Failed to fetch weather data", error))
+        }
+      }, [selectedCountry])
+
     if (selectedCountry) {
       return (
         <div>
@@ -12,6 +32,15 @@ const SelectedCountry = ( {selectedCountry} ) => {
             {Object.values(selectedCountry.languages).map((language, index) => <li key={index}>{language}</li>)}          
           </ul>
           <p style={{ fontSize: '100px' }}>{selectedCountry.flag}</p>
+          <h3>Weather in {selectedCountry.capital}</h3>
+          {weather && (
+            <div>
+                <p>Temperature: {weather.main.temp} °C</p>
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="Weather icon" />
+                <p>Wind Speed: {weather.wind.speed} m/s</p>
+            </div>
+          )}
+
         </div>
       )
     } 
