5f5bece39dbed02f624fe1df1a3af70df02104fb
tomi <tomislav.varga@icloud.com>
2024-07-12 11:27:28 +0200

    solution exercise 4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
     return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+    const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+    return blogs.find(blog => blog.likes === maxLikes)
+}
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
