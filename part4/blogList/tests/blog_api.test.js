7000998dada5220a472e424d03586412fc7e10cf
tomi <tomislav.varga@icloud.com>
2024-07-16 10:50:37 +0200

    solution exercise 4.9

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -24,6 +24,16 @@ test('GET /api/blogs returns all blogs', async () => {
     assert.equal(response.body.length, helper.blogsListAll.length)
 })
 
+test('blogs have identifier field id', async () => {
+    const response = await api
+      .get('/api/blogs')
+
+    response.body.forEach(blog => {
+    assert.ok(blog.hasOwnProperty('id'), 'Blog does not have an id field')
+    assert.equal(typeof blog.id, 'string', 'id field is not a string')
+  })
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
