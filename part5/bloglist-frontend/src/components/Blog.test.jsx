025f189c43c2354f58948a2dcb533e5737d607be
tomi <tomislav.varga@icloud.com>
2024-08-17 15:23:21 +0200

    solution exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title and author', () => {
@@ -23,4 +24,29 @@ test('renders blog title and author', () => {
     expect(p).toHaveTextContent(
         'Test Blog Title by Test Blog Author'
     )
+})
+
+test('blog url and likes are shown when view button is clicked', async () => {
+    const blog = {
+        title: 'Test Blog Title',
+        author: 'Test Blog Author',
+        url: 'testblogurl.com',
+        likes: 10,
+        user: {
+            name: 'Test User',
+            id: 'testuser'
+        }
+    }
+
+    const user = {
+        id: 'testuser'
+    }
+
+    render(<Blog blog={blog} user={user}/>)
+
+    const button = screen.getByText('View')
+    await userEvent.click(button)
+
+    expect(screen.getByText('testblogurl.com')).toBeDefined()
+    expect(screen.getByText('Likes: 10')).toBeDefined()
 })
