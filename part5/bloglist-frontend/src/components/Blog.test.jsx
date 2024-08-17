17d84d9b0f63b5c6d11c095e82acdaabb9a732a6
tomi <tomislav.varga@icloud.com>
2024-08-17 15:59:22 +0200

    solution exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -50,3 +50,34 @@ test('blog url and likes are shown when view button is clicked', async () => {
     expect(screen.getByText('testblogurl.com')).toBeDefined()
     expect(screen.getByText('Likes: 10')).toBeDefined()
 })
+
+test('like button clicked twice calls event handler twice', async () => {
+    const blog = {
+      title: 'Test Blog Title',
+      author: 'Test Blog Author',
+      url: 'testblogurl.com',
+      likes: 10,
+      user: {
+        name: 'Test User',
+        id: 'testuser'
+      }
+    }
+  
+    const user = {
+      id: 'testuser'
+    }
+  
+    const mockUpdateBlog = { current: 0 }
+    const updateBlog = () => { mockUpdateBlog.current += 1 }
+  
+    render(<Blog blog={blog} user={user} updateBlog={updateBlog} />)
+  
+    const viewButton = screen.getByText('View')
+    await userEvent.click(viewButton)
+  
+    const likeButton = screen.getByText('Like')
+    await userEvent.click(likeButton)
+    await userEvent.click(likeButton)
+  
+    expect(mockUpdateBlog.current).toBe(2)
+  })
