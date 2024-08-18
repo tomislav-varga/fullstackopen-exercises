a965af1e2d2c8023a2c64f2f0110219dd996af1d
tomi <tomislav.varga@icloud.com>
2024-08-18 12:45:23 +0200

    solution exercise 5.18

--- part5/E2E-tests/cypress/e2e/blog-app.cy.js
+++ part5/E2E-tests/cypress/e2e/blog-app.cy.js
@@ -1,5 +1,11 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    /* const user = {
+      "name": "Test User",
+      "username": "testuser",
+      "password": "testpassword"
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) */
     cy.visit('http://localhost:5173')
   })
 
@@ -8,5 +14,25 @@ describe('Blog app', function() {
     cy.get('#username').should('exist')
     cy.get('#password').should('exist')
     cy.get('#login-button').should('exist')
+  })
+
+  it('succeeds with correct credentials', function() {
+    cy.get('#username').type('testuser')
+    cy.get('#password').type('testpassword')
+    cy.get('#login-button').click()
+
+    cy.contains('Test User logged in')
+  })
+
+  it('fails with wrong credentials', function() {
+    cy.get('#username').type('testuser')
+    cy.get('#password').type('wrongpassword')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'Username or password is incorrect')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+    cy.get('html').should('not.contain', 'Test User logged in')
   })
 })
