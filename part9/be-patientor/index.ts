5b3490db95475572b3555e2e98742734020914af
tomi <tomislav.varga@icloud.com>
2025-07-08 05:59:49 +0200

    solution exercise 9.9

--- part9/be-patientor/index.ts
+++ part9/be-patientor/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+  console.log('fetching patients');
+  res.json([]);
 });
 
 const PORT = 3003;
